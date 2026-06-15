8349ffc39e7adbff87906c0d704d722dedf47abc
Mirella Fernandes <mirellaalmeidafernandes@gmail.com>
2026-06-15 18:17:26 -0300

    Adicionadas as classes Restaurante, Gerente e Cozinha

--- src/Cozinha.java
+++ src/Cozinha.java
@@ -1,47 +1,31 @@
 import java.util.ArrayList;
+/**
+ *
+ * @author mirella
+ */
+public class Cozinha {
+    private ArrayList<PedidoCliente> listaPedidos = new ArrayList<PedidoCliente> ();
 
-public class Cozinha
-{
-    private ArrayList<PedidoCliente> listaPedidos = new ArrayList<PedidoCliente>();
-    private int idFila;
-    
-    public Cozinha()
-    {
-        this.idFila = 0;
+    public Cozinha() {
     }
 
-    public ArrayList<PedidoCliente> verPedidosAbertos()
-    {
-        ArrayList<PedidoCliente> pedidosAbertos = new ArrayList<PedidoCliente>();
-    
-        for(PedidoCliente pedido: this.listaPedidos){
-            if(pedido.getStatusPedido().equalsIgnoreCase("aberto"))
-                pedidosAbertos.add(pedido);
-        }
-        
-        return pedidosAbertos;
+    public ArrayList getListaPedidos() {
+        return listaPedidos;
+    }
+
+    public void setListaPedidos(ArrayList listaPedidos) {
+        this.listaPedidos = listaPedidos;
     }
     
     public void addPedido(PedidoCliente pedido){
-        this.listaPedidos.add(pedido);
+        listaPedidos.add(pedido);
     }
-    
-    public void entregarPedido(){
-        PedidoCliente atual;
-        
-        if(idFila < this.listaPedidos.size()){
-            atual = this.listaPedidos.get(this.idFila);
-            if(atual.getStatusPedido().equalsIgnoreCase("aberto")){
-                atual.setStatusPedido("entregue");
-                this.idFila++;
-            }
-        }
-    }
-    
-    public int getIdFila(){
-        return this.idFila;
-    }
-    public void setIdFila(int idFila){
-        this.idFila = idFila;
-    }
+     public ArrayList<PedidoCliente> verPedidosAbertos(){
+        return listaPedidos;
+     }
+     
+     public void entregarPedido(PedidoCliente pedido){
+         pedido.setStatus("entregue");
+         listaPedidos.remove(pedido);
+     }
 }
--- src/Gerente.java
+++ src/Gerente.java
@@ -1,60 +1,48 @@
-import java.util.ArrayList;
+/**
+ *
+ * @author mirella
+ */
+public class Gerente {
+    private String nome, cpf, email, senha;
 
-public class Gerente
-{
-    private String nome, cpf, email,senha;
-    private Cardapio cardapio;
-    
-    public Gerente(String nome, String cpf, String email, String senha)
-    {
+    public Gerente(String nome, String cpf, String email, String senha) {
         this.nome = nome;
         this.cpf = cpf;
         this.email = email;
         this.senha = senha;
     }
 
-    public void cadastrarCardapio(itemComida item)
-    {
-        cardapio.getListaComida().add(item);
-    }
-    public void cadastrarCardapio(itemBebida item)
-    {
-        cardapio.getlistaBebida().add(item);
-    }
-    
-    public ArrayList<itemCardapio> getCardapio(){
-        return cardapio;
-    }
-    
     public String getNome() {
         return nome;
     }
-    
-    public String getCpf() {
-        return cpf;
-    }
-    
-    public String getEmail() {
-        return email;
-    }
-    
-    public String getSenha() {
-        return senha;
-    }
-    
+
     public void setNome(String nome) {
         this.nome = nome;
     }
-    
+
+    public String getCpf() {
+        return cpf;
+    }
+
     public void setCpf(String cpf) {
         this.cpf = cpf;
     }
-    
+
+    public String getEmail() {
+        return email;
+    }
+
     public void setEmail(String email) {
         this.email = email;
     }
-    
+
+    public String getSenha() {
+        return senha;
+    }
+
     public void setSenha(String senha) {
         this.senha = senha;
     }
+    
+    
 }
