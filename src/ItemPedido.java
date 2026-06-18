94ba2bd3e31bae689b60774c03195b0a629fdda3
BrunoNight <brunobrrt3680@gmail.com>
2026-06-18 15:05:15 -0300

    Add files via upload

--- src/ItemPedido.java
+++ src/ItemPedido.java
@@ -1,29 +1,30 @@
-public class ItemPedido {
+public class ItemPedido
+{
     private ItemCardapio item;
-    private int quantidade;
+    private int qnt;
 
-    public ItemPedido(ItemCardapio item, int quantidade) {
+    public ItemPedido(ItemCardapio item, int qnt) {
         this.item = item;
-        this.quantidade = quantidade;
+        this.qnt = qnt;
     }
 
     public ItemCardapio getItem() {
-        return item;
+        return this.item;
     }
 
-    public void setItem(ItemCardapio item) {
-        this.item = item;
+    public void setItem(ItemCardapio i) {
+        this.item = i;
     }
 
-    public int getQuantidade() {
-        return quantidade;
+    public int getQnt() {
+        return this.qnt;
     }
 
-    public void setQuantidade(int quantidade) {
-        this.quantidade = quantidade;
+    public void setQnt(int q) {
+        this.qnt = q;
     }
-
-    public void adicionarItem() {
-        this.quantidade++;
+    
+    public float calcularSubtotal() {
+        return this.item.getPrecoltem() * this.qnt;
     }
 }
